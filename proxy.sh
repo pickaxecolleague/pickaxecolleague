923f8952c46cd446e46b4f1e8f578cc2dc0f348c
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-11 13:26:59 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -2,19 +2,15 @@
 
 #127.0.0.1:9050
 
-proot=https://www.dropbox.com/s/qhtue5w4rnxlip3/proot.tar.gz?dl=0
-rootfs=https://www.dropbox.com/s/qrzrq6p26684xex/rootfs.tar.xz?dl=0
+proot=https://www.dropbox.com/s/q171q1igu70mdls/proot.tar.gz?dl=0
 
 wget -O proot.tar.gz $proot
-wget -O rootfs.tar.xz $rootfs
 
 tar -xf proot.tar.gz
-tar -xf rootfs.tar.xz
 
 rm -rf *.tar.*
 
 chmod 777 dist/proot
 
-./dist/proot -S . userdel _apt
-nohup ./dist/proot -S . apt install -y --fix-missing tor && tor  &>/dev/null &
+nohup ./dist/proot -S . tor  &>/dev/null &
 #./dist/proot -S . /bin/bash
